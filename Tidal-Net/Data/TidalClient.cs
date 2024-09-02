f006099a0cab16911fa715b902e5f4d74f7ab5f6
fkabreiddy <breiddysubzero@gmai..com>
2024-09-02 14:35:01 -0400

    update on the client constructor

--- Tidal-Net/Data/TidalClient.cs
+++ Tidal-Net/Data/TidalClient.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net.Http.Headers;
+using System.Text;
+using Tidal_Net.Authentication;
 using Tidal_Net.Data.Services;
 
 namespace Tidal_Net.Data;
@@ -7,27 +9,37 @@ public class TidalClient
 {
     
     
-    
     public TidalAlbumServices Album { get; }
     public TidalTrackServices Tracks { get; }
     public TidalArtistServices Artist { get; }
 
-    private readonly HttpClient _httpClient;
+    private static HttpClient _httpClient;
     
     private readonly TidalRequester _requester;
 
     public TidalClient( string token, string market = "us")
     {
-        _httpClient = new HttpClient() ?? throw new ArgumentNullException(nameof(_httpClient));
-        _httpClient.BaseAddress = new Uri("https://openapi.tidal.com/v2/");
-        _httpClient.DefaultRequestHeaders.Accept.Clear();
-        _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.tidal.v1+json"));
-        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+       
+        BuildHttpClient(token);
         _requester = new TidalRequester(_httpClient);
         Album = new TidalAlbumServices(_requester, market);
         Tracks = new TidalTrackServices(_requester, market);
         Artist = new TidalArtistServices(_requester, market);
     }
+  
+
+    private void BuildHttpClient(string token)
+    {
+        
+        _httpClient = new HttpClient() ?? throw new ArgumentNullException(nameof(_httpClient));
+        _httpClient.BaseAddress = new Uri("https://openapi.tidal.com/v2/");
+        _httpClient.DefaultRequestHeaders.Accept.Clear();
+        _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.tidal.v1+json"));
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+    }
+    
+    
     
   
 }
